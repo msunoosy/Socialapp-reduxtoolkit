7e70c5fc3386ebfc2079855be7129aaa1a0e1e27
zamzamy Mohamed sunoosy <m.sunoos@gmail.com>
2024-10-09 16:29:14 +0400

    Single post page bugs fixed

--- My app/src/features/posts/PostsExcerpt.js
+++ My app/src/features/posts/PostsExcerpt.js
@@ -1,6 +1,7 @@
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
+import { Link } from "react-router-dom";
 
 const PostsExcerpt = ({ post }) => {
     return (
@@ -8,6 +9,7 @@ const PostsExcerpt = ({ post }) => {
             <h3>{post.title}</h3>
             <p>{post.body.substring(0, 100)}</p>
             <p className="postCredit">
+                <Link to={`post/${post.id}`}> View Post</Link>
                 <PostAuthor userId={post.userId} />
                 <TimeAgo timestamp={post.date} />
             </p>
--- My app/src/features/posts/SinglePostPage.js
+++ My app/src/features/posts/SinglePostPage.js
@@ -1,35 +1,29 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { selectPostById } from './postsSlice'
-import PostAuthor from './PostAuthor'
-import TimeAgo from './TimeAgo'
-import ReactionButtons from './ReactionButtons'
-import { useParams } from 'react-router-dom'
-
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectPostById } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
+import { useParams } from "react-router-dom";
 
 function SinglePostPage() {
-    const {postId}=useParams()
-    const post=useSelector((state)=>selectPostById(state,Number(postId)))
-  if(!post){
+  const { postId } = useParams();
+  const post = useSelector((state) => selectPostById(state, Number(postId)));
+  if (!post) {
+    return <section>Post not found!!!</section>
+  }
     return (
-    <section>
-        Post not found!!!
-    </section>
-  )
-return(
-    <article>
-    <h3>{post.title}</h3>
-    <p>{post.body}</p>
-    <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-    </p>
-    <ReactionButtons post={post} />
-</article>
-)
-
-}
+      <article>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+        <p className="postCredit">
+          <PostAuthor userId={post.userId} />
+          <TimeAgo timestamp={post.date} />
+        </p>
+        <ReactionButtons post={post} />
+      </article>
+    );
+  
 }
 
-export default SinglePostPage
+export default SinglePostPage;
