7393a55ba49ddabd7608cb28e0386a176b3dc6bc
zamzamy Mohamed sunoosy <m.sunoos@gmail.com>
2024-10-11 01:32:56 +0400

    use effect removed

--- My app/src/index.js
+++ My app/src/index.js
@@ -6,7 +6,9 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { fetchUsers } from './features/users/usersSlice';
 import { Route, BrowserRouter as Router,Routes } from 'react-router-dom';
+import { fetchPosts } from './features/posts/postsSlice';
 
+store.dispatch(fetchPosts())
 store.dispatch(fetchUsers())
 ReactDOM.render(
   <React.StrictMode>
